d3cc3d1547ba9092adfa8595a5f7a75f47a4fc4d
Pankaj vishw <vishw.dev.1000@gmail.com>
2024-12-08 15:22:59 +0000

    (Backend) Initialized Project and configged AWS S3
    Initialized Project
    
    Configged AWS S3 bucket with django-storages
    to store media files into s3 bucket.

---
+++ backend/careerahead/settings/__init__.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import dotenv
+import os
+
+dotenv.load_dotenv()
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+
+# By default our project runs of Prod mode until specified 
+DEBUG = os.getenv("DEBUG", "FALSE").lower() in ('true', 't', '1')
+
+from .base import *
+
+if DEBUG:
+    from .dev import * 
+else:
+    from .prod import * 
---
+++ backend/careerahead/settings/dev.py
@@ -0,0 +1,17 @@
+
+from . import BASE_DIR
+import os 
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+    }
+}
